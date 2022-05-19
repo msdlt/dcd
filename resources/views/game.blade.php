4d77f6cb0b3372ba9be5958a826becde9a79ed1f
Damion Young <allotmentor@allotmentor.com>
2022-05-19 20:41:54 +0100

    first commit

---
+++ resources/views/game.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+      <script src="/js/app.js"></script>
+    </head>
+    <body class="antialiased">
+        
+        
+    </body>
+</html>
